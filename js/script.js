9f2f0534cf5d4dcb26f5e6305a14fb1e12d96898
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-28 17:43:27 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -11,17 +11,17 @@ console.log(price);
 // 2. va applicato uno sconto del 20% per i minorenni
 // 3. va applicato uno sconto del 40% per gli over 65.
 if (passengerAge <= 18) {
-    let discountYoung = Number((kmNumber * 0.21) * (4 / 5))
+    discountYoung = Number((kmNumber * 0.21) * (4 / 5))
     discountYoung = discountYoung.toFixed(2);
     console.log(discountYoung);
     document.getElementById("ticketprice").innerHTML = `Prezzo biglietto (young18): ${discountYoung}€`
 } else if (passengerAge >= 65) {
-    let discountOld = Number((kmNumber * 0.21) * (3 / 5))
+    discountOld = Number((kmNumber * 0.21) * (3 / 5))
     discountOld = discountOld.toFixed(2);
     console.log(discountOld);
     document.getElementById("ticketprice").innerHTML = `Prezzo biglietto (old65): ${discountOld}€`
 } else {
-    let price = Number(kmNumber * 0.21)
+    price = Number(kmNumber * 0.21)
     price = price.toFixed(2);
     console.log(price);
     document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${price}€`;
