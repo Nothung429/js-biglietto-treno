c858332bab8dc9336b2e9faad6a5426692cffbc9
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-28 16:19:04 +0200

    aggiunto arrotondamento

--- js/script.js
+++ js/script.js
@@ -1,23 +1,26 @@
 // Chiedere i kilometri del viaggio
-const Kmnumber = Number(prompt("Inserire numero di chilometri desiderato"));
-console.log(Kmnumber);
+const kmNumber = Number(prompt("Inserire numero di chilometri desiderato"));
+console.log(kmNumber);
 // Chiedere l'età del passeggero
 const passengerAge = Number(prompt("Inserire eta del passeggero"));
 console.log(passengerAge);
 // 1. il prezzo del biglietto base è definito in base ai km (0.21 € al km)
-let price = Number(Kmnumber * 0.21)
+let price = Number(kmNumber * 0.21)
+price = price.toFixed(2);
 console.log(price);
 document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${price}€`
 // 2. va applicato uno sconto del 20% per i minorenni
 if (passengerAge <= 18) {
-    let discountYoung = Number((Kmnumber * 0.21) * (4 / 5))
+    let discountYoung = Number((kmNumber * 0.21) * (4 / 5))
+    discountYoung = discountYoung.toFixed(2);
     console.log(discountYoung);
-    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${discountYoung}€`
+    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto (young18): ${discountYoung}€`
 }
 // 3. va applicato uno sconto del 40% per gli over 65.
 if (passengerAge >= 65) {
-    let discountOld = Number((Kmnumber * 0.21) * (3 / 5))
+    let discountOld = Number((kmNumber * 0.21) * (3 / 5))
+    discountOld = discountOld.toFixed(2);
     console.log(discountOld);
-    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${discountOld}€`
+    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto (old65): ${discountOld}€`
 }
-// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
+// Prezzo finale con massimo due cifre decimali
