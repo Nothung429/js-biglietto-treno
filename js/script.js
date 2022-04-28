67f5768694835153c230c1a361cc3dd77d8793c8
Nothung429 <frank.gatto96.fg@gmail.com>
2022-04-28 15:52:06 +0200

    fix generali e inizio arrotondamento

--- js/script.js
+++ js/script.js
@@ -1,20 +1,23 @@
-// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Chiedere i kilometri del viaggio
 const Kmnumber = Number(prompt("Inserire numero di chilometri desiderato"));
 console.log(Kmnumber);
+// Chiedere l'età del passeggero
 const passengerAge = Number(prompt("Inserire eta del passeggero"));
 console.log(passengerAge);
-// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
-// 1. il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// 1. il prezzo del biglietto base è definito in base ai km (0.21 € al km)
 let price = Number(Kmnumber * 0.21)
 console.log(price);
+document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${price}€`
 // 2. va applicato uno sconto del 20% per i minorenni
 if (passengerAge <= 18) {
     let discountYoung = Number((Kmnumber * 0.21) * (4 / 5))
     console.log(discountYoung);
+    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${discountYoung}€`
 }
 // 3. va applicato uno sconto del 40% per gli over 65.
 if (passengerAge >= 65) {
     let discountOld = Number((Kmnumber * 0.21) * (3 / 5))
     console.log(discountOld);
+    document.getElementById("ticketprice").innerHTML = `Prezzo biglietto: ${discountOld}€`
 }
 // L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca.
